7247dd06579c88307a8a7dd26c8f69ceae2adf7e
Elliot <elliot.xu@coherencedigital.com>
2023-04-07 15:23:17 +0800

    feat: gui size and some other config

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -5,6 +5,27 @@
 
 ;;; Code:
 
+;; 禁用一些GUI特性
+(setq use-dialog-box nil)               ; 鼠标操作不使用对话框
+(setq inhibit-default-init t)           ; 不加载 `default' 库
+(setq inhibit-startup-screen t)         ; 不加载启动画面
+(setq inhibit-startup-message t)        ; 不加载启动消息
+(setq inhibit-startup-buffer-menu t)    ; 不显示缓冲区列表
+
+;; 设置窗口大小，仅仅在图形界面需要设置
+(when (display-graphic-p)
+  (let ((top    0)                                     ; 顶不留空
+        (left   (/ (x-display-pixel-width) 10))        ; 左边空10%
+        (height (round (* 0.8                          ; 窗体高度为0.8倍的显示高度
+                          (/ (x-display-pixel-height)
+                             (frame-char-height))))))
+    (let ((width  (round (* 2.5 height))))             ; 窗体宽度为2.5倍高度
+      (setq default-frame-alist nil)
+      (add-to-list 'default-frame-alist (cons 'top top))
+      (add-to-list 'default-frame-alist (cons 'left left))
+      (add-to-list 'default-frame-alist (cons 'height height))
+      (add-to-list 'default-frame-alist (cons 'width width)))))
+
 (electric-pair-mode t)                       ; 自动补全括号
 (column-number-mode t)                       ; 在 Mode line 上显示列号
 (global-auto-revert-mode t)                  ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
