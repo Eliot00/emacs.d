1a9760f4c6e88584c57fcca06c2106a881a4b84e
Elliot <elliot.xu@coherencedigital.com>
2023-04-10 17:36:54 +0800

    chore: use el-themes instead of solo-jazz

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -5,7 +5,51 @@
 
 ;;; Code:
 
-(load-theme 'solo-jazz t)
+(use-package ef-themes
+  :ensure nil
+  :init
+  ;; set two specific themes and switch between them
+  (setq ef-themes-to-toggle '(ef-summer ef-winter))
+  ;; set org headings and function syntax
+  (setq ef-themes-headings
+        '((0 . (bold 1))
+          (1 . (bold 1))
+          (2 . (rainbow bold 1))
+          (3 . (rainbow bold 1))
+          (4 . (rainbow bold 1))
+          (t . (rainbow bold 1))))
+  (setq ef-themes-region '(intense no-extend neutral))
+  ;; Disable all other themes to avoid awkward blending:
+  (mapc #'disable-theme custom-enabled-themes)
+
+  ;; Load the theme of choice:
+  ;; The themes we provide are recorded in the `ef-themes-dark-themes',
+  ;; `ef-themes-light-themes'.
+
+  ;; 如果你不喜欢随机主题，也可以直接固定选择一个主题，如下：
+  ;; (ef-themes-select 'ef-summer)
+
+  ;; 随机挑选一款主题，如果是命令行打开Emacs，则随机挑选一款黑色主题
+  (if (display-graphic-p)
+      (ef-themes-load-random)
+    (ef-themes-load-random 'dark))
+
+  :config
+  ;; auto change theme, aligning with system themes.
+  (defun my/apply-theme (appearance)
+    "Load theme, taking current system APPEARANCE into consideration."
+    (mapc #'disable-theme custom-enabled-themes)
+    (pcase appearance
+      ('light (if (display-graphic-p) (ef-themes-load-random 'light) (ef-themes-load-random 'dark)))
+      ('dark (ef-themes-load-random 'dark))))
+
+  (if (eq system-type 'darwin)
+      ;; only for emacs-plus
+      (add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+    (ef-themes-select 'ef-summer)
+    )
+  )
+
 (use-package doom-modeline
   :ensure nil
   :hook (after-init . doom-modeline-mode)
